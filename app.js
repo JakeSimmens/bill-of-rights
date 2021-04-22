41a718b6fcda537751f0de065bc0d1fc20d80665
Jake Simmens <rocknjake@gmail.com>
2021-04-21 20:17:08 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const {EMAIL_USERNAME, EMAIL_PASSWORD, SESSION_SECRET} = require("./config.js");
+
+const express    = require("express");
+const app        = express();
+const bodyParser = require("body-parser");
+//const nodemailer = require("nodemailer");
+//const favicon    = require("serve-favicon");
+const session    = require("express-session");
+const flash      = require("connect-flash");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+//app.use(favicon(__dirname + "/public/images/favicon.ico"));
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+
+
+app.get('/', (req, res) => {
+    // res.render("index", {
+    //   messages: req.flash("info"),
+    //   contactMessage: ""
+    // });
+    res.send("Site works");
+});
+
+const port = process.env.PORT || 3000;
+app.server = app.listen(port, function startServer() {
+    console.log("portfolio server running");
+});
---
+++ config.js
@@ -0,0 +1,9 @@
+const {SESSION_SECRET}  = require("./secrets.js");
+
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  //EMAIL_USERNAME: process.env.EMAIL_USERNAME || EMAIL_USERNAME,
+  //EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || EMAIL_PASSWORD,
+  SESSION_SECRET: process.env.SESSION_SECRET || SESSION_SECRET
+}
